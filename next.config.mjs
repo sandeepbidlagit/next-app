813eafefec1b9c61e9ab214c544eaf64d6d3e01f
sandeepbidlagit <sandeep.bidla556@gmail.com>
2026-05-29 17:57:49 +0530

    fix github static paths

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,12 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
   output: "export",
- images: {
+
+  images: {
     unoptimized: true,
   },
-  basePath: '/next-app',
-  assetPrefix: '/next-app/',
+
+  basePath: "/next-app",
+  assetPrefix: "/next-app/",
 };
 
 export default nextConfig;
