de15e6c73cf98c1bb06ceca1d1b5a26c0ae32fe6
sandeepbidlagit <sandeep.bidla556@gmail.com>
2026-05-30 08:58:26 +0530

    Updates

--- _next/static/Zi9jPW342gf8GeTMZ91rj/_buildManifest.js
+++ _next/static/Zi9jPW342gf8GeTMZ91rj/_buildManifest.js
@@ -1,12 +1,7 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [
-      {
-        "source": "/next-app//_next/:path+",
-        "destination": "/next-app/_next/:path+"
-      }
-    ],
+    "beforeFiles": [],
     "fallback": []
   },
   "sortedPages": [
